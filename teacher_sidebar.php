73844149c522b0ed48442754add841d57391e49e
Tommy Tan <tanjunlin0215@outlook.com>
2023-11-01 00:22:25 +0800

    Add files via upload

---
+++ teacher_sidebar.php
@@ -0,0 +1,60 @@
+<div class="navigation active">
+    <ul>
+        <li>
+            <a href="">
+                <span class="icon">
+                    <img src="../assets/Images/logo.png" alt="Logo" width="50" height="50">
+                </span>
+                <h4 class="navigation_title">AlwaysAStudent!</h4>
+            </a>
+            <hr>
+        </li>
+
+        <li>
+            <a href="../index.php" >
+                <span class="icon" >
+                    <i class='bx bx-home-alt-2'></i>
+                </span>
+                <span class="title">Home</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="dashboard.php">
+                <span class="icon" >
+                    <ion-icon name="folder-open-outline"></ion-icon>
+                </span>
+                <span class="title">Course</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="comunication.php" >
+                <span class="icon" >
+                    <ion-icon name="chatbox-ellipses-outline"></ion-icon>
+                </span>
+                <span class="title">Comunication</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="performance.php">
+                <span class="icon">
+                    <ion-icon name="bar-chart-outline" ></ion-icon>
+                </span>
+                <span class="title">Performance</span>
+            </a>
+        </li>
+
+         <li>
+            <a href="#">
+                <span class="icon">
+                    <ion-icon name="hammer-outline"></ion-icon>
+                </span>
+                <span class="title">Tools</span>
+            </a>
+        </li> 
+
+    </ul>
+</div>
+
---
+++ teacher_topnav_session.php
@@ -0,0 +1,36 @@
+<?php 
+	require('include/dbconn.php');
+
+	if (!isset($_SESSION['TcMail'])|| !isset($_SESSION['TcPass'])) {
+        header('location: ../index.php');
+    }
+
+    $name = $_SESSION['TcName'];
+	$email = $_SESSION['TcMail'];
+	$sql_statement1 = "SELECT * FROM users WHERE UsrEmail='$email'";
+
+    $execqry1 = mysqli_query($conn,$sql_statement1);
+		
+	while ($rowresult = mysqli_fetch_assoc($execqry1)){
+		$photo=$rowresult['UsrImage'];
+		$name2 = $rowresult['UsrName'];
+    }
+
+    if(empty($photo)){
+		$image="../assets/Images/user.png";
+	}else{
+		$image="data:image;base64,".$photo;
+	}
+?>
+
+<div class="topbar">
+    <ion-icon name="menu-outline" class="toggle"></ion-icon>
+    <div>
+        <img src="<?php echo $image ?>" alt="" class="user" id="user"/>
+    </div>
+    <div class="user_submenu">
+        <span class="submenu_item"><a href="accPage.php"><ion-icon name="person-outline"></ion-icon> &emsp; Profile</a></span>
+        <span class="submenu_item"><a href="process_logout.php"><ion-icon name="log-out-outline"></ion-icon> &emsp; Logout</a></span>
+    </div>
+</div>
+
